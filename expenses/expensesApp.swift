e4267a9872ed949ad40d93951449aa394a514d5f
pavel-ryzhov <pavelryzhov06@gmail.com>
2024-07-14 02:18:57 +0300

    Initial Commit

---
+++ expenses/expensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensesApp.swift
+//  expenses
+//
+//  Created by pavel on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct expensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
